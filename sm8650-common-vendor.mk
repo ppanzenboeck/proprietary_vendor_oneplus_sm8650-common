a5dd39bcde2eb689d00e52f73f7ef18f7f1cf128
Arian <arian.kulmer@web.de>
2025-07-22 16:02:11 +0530

    sm8650-common: Enable ELF checks for audio blobs
    Change-Id: I6d7e071ed03411e809358f57879404a047b9bb0f

--- sm8650-common-vendor.mk
+++ sm8650-common-vendor.mk
@@ -737,7 +737,6 @@ PRODUCT_PACKAGES += \
     libvpplibrary \
     libvui_dmgr \
     libvui_dmgr_client \
-    libvui_intf \
     libwfdcodecv4l2_proprietary \
     libwfdcommonutils_proprietary \
     libwfdconfigutils_proprietary \
